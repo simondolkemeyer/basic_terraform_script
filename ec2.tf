f5fd3ff9aa4bdb78564b10b2fa8896e9bd4ea7b9
Simon <simon@S.local>
2023-03-29 09:46:10 +0200

    ec2 corecctions

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "My_ec2_instance" {
   ami                     = var.ami
   instance_type           = "t3.micro"
-  subnet_id               = aws_subnet.public_subnet_1
-  vpc_security_group_ids  = [aws_security_group.My_VPC_Security_Group.id]
+  subnet_id               = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids  = [aws_security_group.My_VPC_Security_group.id]
   key_name                = var.key
   associate_public_ip_address = true
   user_data = "${file("install_wp.sh")}"
