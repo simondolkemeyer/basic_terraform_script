3d8b40a3fc83f5030a3875849ba933b051cb2a8a
Simon <simon@S.local>
2023-03-30 17:13:50 +0200

    wp script

--- install_wp.sh
+++ install_wp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -x
 HOMEDIR=/home/ec2-user
-sudo yum update -y
-sudo amazon-linux-extras install lamp-mariadb10.2-php7.2 -y
+yum update -y
+amazon-linux-extras install lamp-mariadb10.2-php7.2
 echo Installing packages...
 echo Please ignore messages regarding SELinux...
-sudo yum install -y \
+yum install -y \
 httpd \
 mariadb-server \
 php \
@@ -14,24 +13,24 @@ php-mbstring \
 php-mysqlnd \
 php-xml \
 php-xmlrpc
-MYSQL_ROOT_PASSWORD=password
+MYSQL_ROOT_PASSWORD=pass
 echo $MYSQL_ROOT_PASSWORD > $HOMEDIR/MYSQL_ROOT_PASSWORD
-sudo chown ec2-user $HOMEDIR/MYSQL_ROOT_PASSWORD
+chown ec2-user $HOMEDIR/MYSQL_ROOT_PASSWORD
 echo Starting database service...
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 echo Setting up basic database security...
-mysql -u root <<DB_SEC
+mysql -u root <<EOF
 UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
-DB_SEC
+EOF
 echo Configuring Apache...
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec chmod 2775 {} \;
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www && find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 echo Starting Apache...
 sudo systemctl start httpd
@@ -39,27 +38,26 @@ sudo systemctl enable httpd
 echo installing wordpress and creating mysqluser
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz -C /home/ec2-user
-
-mkdir /home/ec2-user/.aws
-touch /home/ec2-user/.aws/config
-cat << END > /home/ec2-user/.aws/config
-[default]
-region = us-west-2
-output = yaml
-END
-
 sudo systemctl start mariadb
-
-echo configuring wordpress
-echo $dbendpoint
-database_name=mydb
-master_username=root
-master_password=password
-sudo cp /home/ec2-user/wordpress/wp-config-sample.php /home/ec2-user/wordpress/wp-config.php
-sudo sed -i "s/database_name_here/$database_name/" /home/ec2-user/wordpress/wp-config.php
-sudo sed -i "s/username_here/$master_username/" /home/ec2-user/wordpress/wp-config.php
-sudo sed -i "s/password_here/$master_password/" /home/ec2-user/wordpress/wp-config.php
-sudo sed -i "s/localhost/${dbendpoint}/" /home/ec2-user/wordpress/wp-config.php
-
-sudo cp -r /home/ec2-user/wordpress/* /var/www/html/
+DB_USER="wordpressuser"
+DB_PASSWORD="pass"
+DB_NAME="wordpressdb"
+# create user and grant privileges
+mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+cp /home/ec2-user/wordpress/wp-config-sample.php /home/ec2-user/wordpress/wp-config.php
+sed -i "s/database_name_here/wordpressdb/" /home/ec2-user/wordpress/wp-config.php
+sed -i "s/username_here/wordpressuser/" /home/ec2-user/wordpress/wp-config.php
+sed -i "s/password_here/pass/" /home/ec2-user/wordpress/wp-config.php
+cp -r /home/ec2-user/wordpress/* /var/www/html/
 sudo systemctl restart httpd
+/opt/aws/bin/cfn-signal \
+-e $? \
+--stack ${AWS::StackName} \
+--resource LampInstance \
+--region ${AWS::Region}
+EOF
